847ce34a1633aee1ca3d929d06689479241afcd0
[Keval Patel] <[khp90611@gmail.com]>
2023-08-30 10:26:56 -0400

    Finished put for category routes. Not sure if ti works yet. Still need to figure out how to console log successful or not with if statement

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,12 +38,25 @@ router.post('/', async (req, res) => {
   })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  
-});
+  try {
+    const categoryData = await Category.findByPk(req.body,{
+      where: {
+        id: req.params.id,
+      }
+      
+    })
 
-router.delete('/:id', async (req, res) => {
+    
+
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+  });
+  
+  router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   const categoryData = await Category.destroy({
     where: {
